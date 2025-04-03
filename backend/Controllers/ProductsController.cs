3131478c488a24171bd8eb2640bc6f9764296222
Branden Min <brandenmin@gmail.com>
2025-04-03 00:26:32 -0400

    feat: update products controller

--- backend/Controllers/ProductsController.cs
+++ backend/Controllers/ProductsController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using backend.Data;
 using backend.Models;
+using backend.Services;
+using backend.Services.Interfaces;
 
 namespace backend.Controllers
 {
@@ -14,95 +13,89 @@ namespace backend.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IProductService _productService;
 
-        public ProductsController(ApplicationDbContext context)
+        public ProductsController(IProductService productService)
         {
-            _context = context;
+            _productService = productService;
         }
 
-        // GET: api/Products
+        /// <summary>
+        /// Get all products.
+        /// Endpoint: GET api/products
+        /// </summary>
+        /// <returns>List of products</returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
-            return await _context.Products.ToListAsync();
+            return Ok(await _productService.GetAllProducts());
         }
 
-        // GET: api/Products/5
+        /// <summary>
+        /// Get a product by ID.
+        /// Endpoint: GET api/products/{id}
+        /// </summary>
+        /// <param name="id">Product ID</param>
+        /// <returns>The product with the specified ID</returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await _context.Products.FindAsync(id);
+            var product = await _productService.GetProductById(id);
 
             if (product == null)
             {
                 return NotFound();
             }
 
-            return product;
+            return Ok(product);
         }
 
-        // PUT: api/Products/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        /// <summary>
+        /// Create a new product.
+        /// Endpoint: POST api/products
+        /// </summary>
+        /// <param name="product">Product object</param>
+        /// <returns>The created product</returns>
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            await _productService.CreateProduct(product);
+            return CreatedAtAction(nameof(GetProduct), new { id = product.ProductId }, product);
+        }
+
+        /// <summary>
+        /// Update an existing product.
+        /// Endpoint: PUT api/products/{id}
+        /// </summary>
+        /// <param name="id">Product ID</param>
+        /// <param name="product">Updated product object</param>
+        /// <returns>No content</returns>
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutProduct(int id, Product product)
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
         {
             if (id != product.ProductId)
             {
                 return BadRequest();
             }
 
-            _context.Entry(product).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ProductExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
+            await _productService.UpdateProduct(product);
             return NoContent();
         }
 
-        // POST: api/Products
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Product>> PostProduct(Product product)
-        {
-            _context.Products.Add(product);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
-        }
-
-        // DELETE: api/Products/5
+        /// <summary>
+        /// Delete a product by ID.
+        /// Endpoint: DELETE api/products/{id}
+        /// </summary>
+        /// <param name="id">Product ID</param>
+        /// <returns>No content</returns>
         [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> DeleteProduct(int id)
         {
-            var product = await _context.Products.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            _context.Products.Remove(product);
-            await _context.SaveChangesAsync();
-
+            await _productService.DeleteProduct(id);
             return NoContent();
-        }
-
-        private bool ProductExists(int id)
-        {
-            return _context.Products.Any(e => e.ProductId == id);
         }
     }
 }
